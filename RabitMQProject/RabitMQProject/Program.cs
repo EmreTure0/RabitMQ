6ad693528c6a42a32151a9e61a2a7338e0029d78
EmreTure0 <33383123+EmreTure0@users.noreply.github.com>
2019-04-10 02:23:55 +0300

    C# RabitMQ Publisher - Consumer
    C# RabitMQ Publisher - Consumer

---
+++ RabitMQProject/RabitMQProject/Program.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabitMQProject
+{
+    class Program
+    {
+        public class Log
+        {
+            public int LogID { get; set; }
+            public string Message { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            List<Log> pr = new List<Log>();
+            for (int i = 0; i < 88000; i++)
+            {
+                pr.Add(new Log
+                {
+                    LogID = i,
+                    Message = "RabitMQ Test"
+                });
+            }
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (IConnection connection = factory.CreateConnection())
+            using (IModel channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "RabitMQTest",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string message = JsonConvert.SerializeObject(pr);
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "RabitMQTest",
+                                     basicProperties: null,
+                                     body: body);
+            }
+
+            Console.WriteLine("Loglar Gönderildi");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabitMQProject/RabitMQRecevied/Program.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabitMQRecevied
+{
+    class Program
+    {
+        public class Log
+        {
+            public int LogID { get; set; }
+            public string Message { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (IConnection connection = factory.CreateConnection())
+            using (IModel channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "RabitMQTest",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    var person = JsonConvert.DeserializeObject<List<Log>>(message);
+                    Console.Write(message);
+                };
+                channel.BasicConsume(queue: "RabitMQTest",
+                                     autoAck: true,
+                                     consumer: consumer);
+
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
